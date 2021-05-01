18c3758e6924e74f54951075e7addf7d80cb5cae
Piotr Zawadzki <piotr.98zawadzki@gmail.com>
2021-05-01 20:44:38 +0200

    Calculate BMI rating

--- src/pl/nauka/BmiCaltulator.java
+++ src/pl/nauka/BmiCaltulator.java
@@ -1,4 +1,18 @@
 package pl.nauka;
 
 public class BmiCaltulator {
+    private  int weight;
+    private int height;
+
+
+    public BmiCaltulator(int weight, int height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public float countBMI(){
+
+        return  (float) weight/ ((float)height*(float) height)*10000;
+
+    }
 }
--- src/pl/nauka/Main.java
+++ src/pl/nauka/Main.java
@@ -1,8 +1,23 @@
 package pl.nauka;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        calculateBMI();
+    }
+
+    static public void calculateBMI(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, press your weight: ");
+        int weight = scanner.nextInt();
+        System.out.println("Press your height now: ");
+        int height = scanner.nextInt();
+         BmiCaltulator bmiCaltulator = new BmiCaltulator(weight,height);
+        System.out.println("Your BMI is equals to: " + bmiCaltulator.countBMI());
+
+
     }
 }
